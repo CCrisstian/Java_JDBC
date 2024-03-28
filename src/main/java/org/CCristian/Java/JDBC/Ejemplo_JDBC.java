c2144b8b88b864ba0736f91e2a53b92462260ff9
CCrisstian <crisstiann.c@gmail.com>
2024-03-28 16:06:29 -0300

    Primer paso para implementar el patron SINGLETON en la Base de Datos

--- src/main/java/org/CCristian/Java/JDBC/Ejemplo_JDBC.java
+++ src/main/java/org/CCristian/Java/JDBC/Ejemplo_JDBC.java
@@ -1,29 +1,28 @@
 package org.CCristian.Java.JDBC;
 
+import org.CCristian.Java.JDBC.Util.Conexion_BaseDeDatos_SINGLETON;
+
 import java.sql.*;
 
 public class Ejemplo_JDBC {
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=America/Argentina/Buenos_Aires";
-        String user_name = "root";
-        String password = "sasa";
-
-        try {
-            Connection connection = DriverManager.getConnection(url, user_name, password);
-            /*Conectarse a la base de datos*/
-            Statement statement = connection.createStatement();
-            /*Poder ejecutar acciones en la base de datos*/
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM productos");
-            /*Obtiene de la Base de Datos la planilla 'productos'*/
-
+        try (Connection connection = Conexion_BaseDeDatos_SINGLETON.getInstance();
+        /*Conectarse a la base de datos*/
+             Statement statement = connection.createStatement();
+        /*Poder ejecutar acciones en la base de datos*/
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM productos")
+        /*Obtiene de la Base de Datos la planilla 'productos'*/)
+        {
+            System.out.println("----------------------------------------------");
+            System.out.println("id\t|\tnombre\t|\tprecio\t|\tfecha_registro");
+            System.out.println("----------------------------------------------");
             while (resultSet.next()){
-                System.out.println(resultSet.getString("nombre"));
+                System.out.print(resultSet.getInt("id") + "\t|\t");
+                System.out.print(resultSet.getString("nombre") + "\t|\t");
+                System.out.print(resultSet.getInt("precio") + "\t|\t");
+                System.out.println(resultSet.getString("fecha_registro"));
             }
-            resultSet.close();
-            statement.close();
-            connection.close();
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
